a800c5f7c988f6078b8042fddb4042337f4beb9a
Benjamin Taylor <taylorbf@gmail.com>
2020-10-10 12:43:49 -0400

    meta grouping, compose with group, and include quiet samples

--- src/components/Player.js
+++ src/components/Player.js
@@ -3,43 +3,41 @@ import React, { useEffect } from 'react';
 import composition from '../scores/dev.js'
 
 
-const filter = new Tone.Filter(1500, "lowpass").toDestination();
+const filter = new Tone.Filter(1300, "lowpass").toDestination();
 
 const sampler = new Tone.Sampler({
   urls: {
-    A0: "A0.mp3",
-    C1: "C1.mp3",
-    "D#1": "Ds1.mp3",
-    "F#1": "Fs1.mp3",
-    A1: "A1.mp3",
-    C2: "C2.mp3",
-    "D#2": "Ds2.mp3",
-    "F#2": "Fs2.mp3",
-    A2: "A2.mp3",
-    C3: "C3.mp3",
-    "D#3": "Ds3.mp3",
-    "F#3": "Fs3.mp3",
-    A3: "A3.mp3",
-    C4: "C4.mp3",
-    "D#4": "Ds4.mp3",
-    "F#4": "Fs4.mp3",
-    A4: "A4.mp3",
-    C5: "C5.mp3",
-    "D#5": "Ds5.mp3",
-    "F#5": "Fs5.mp3",
-    A5: "A5.mp3",
-    C6: "C6.mp3",
-    "D#6": "Ds6.mp3",
-    "F#6": "Fs6.mp3",
-    A6: "A6.mp3",
-    C7: "C7.mp3",
-    "D#7": "Ds7.mp3",
-    "F#7": "Fs7.mp3",
-    A7: "A7.mp3",
-    C8: "C8.mp3"
+    C1: "C1v1.mp3",
+    "D#1": "Ds1v1.mp3",
+    "F#1": "Fs1v1.mp3",
+    A1: "A1v1.mp3",
+    C2: "C2v1.mp3",
+    "D#2": "Ds2v1.mp3",
+    "F#2": "Fs2v1.mp3",
+    A2: "A2v1.mp3",
+    C3: "C3v1.mp3",
+    "D#3": "Ds3v1.mp3",
+    "F#3": "Fs3v1.mp3",
+    A3: "A3v1.mp3",
+    C4: "C4v1.mp3",
+    "D#4": "Ds4v1.mp3",
+    "F#4": "Fs4v1.mp3",
+    A4: "A4v1.mp3",
+    C5: "C5v1.mp3",
+    "D#5": "Ds5v1.mp3",
+    "F#5": "Fs5v1.mp3",
+    A5: "A5v1.mp3",
+    C6: "C6v1.mp3",
+    "D#6": "Ds6v1.mp3",
+    "F#6": "Fs6v1.mp3",
+    A6: "A6v1.mp3",
+    C7: "C7v1.mp3",
+    "D#7": "Ds7v1.mp3",
+    "F#7": "Fs7v1.mp3",
+    A7: "A7v1.mp3",
   },
   release: 1,
-  baseUrl: "https://tonejs.github.io/audio/salamander/"
+  baseUrl: process.env.PUBLIC_URL + '/audio/'
 }).connect(filter)
 
 const initAudio = async () => {
--- src/scores/04.js
+++ src/scores/04.js
@@ -8,8 +8,6 @@ let a = new Group([
   new Note(495, 2000, 0.5),
   new Note(435, 0, 0.3),
   new Note(330, 1000, 0.5),
-  new Note(220, 1000, 0.4),
-  new Note(550, 400, 0.2),
 ])
 a = a.times(1/2)
 // a = a.compress(1/4)
@@ -17,15 +15,10 @@ a = a.times(1/2)
 console.log(a);
 
 let intervals = [9/8, 3/2, 4/3, 7/4, 2/1, 3/1]
-let rates = [1/3, 1.3, 4, 1/3, 2, 1.6, 1.1, 1, 1/3, 1.3, 1/3, 1.6, 1.1, 1 ]
+let rates = [1, 1.3, 1.6, 5, 1/2]
 let reps = [1, 2, 3]
 
-const rm = 7.4321//0.73 // random multiplier
-const rx = index => (index % rm) / rm;
-
 const pick = arr => arr[Math.floor(Math.random()*arr.length)]
-
-const picki = (arr, index) => arr[Math.floor(rx(index)*arr.length)]
 
 const loop = (amt, func) => {
   let events = []
@@ -35,12 +28,11 @@ const loop = (amt, func) => {
   return events
 }
 
-
 const composition = new Group(
-  loop(20, index => {
-    const start = rx(index)
-    const end = rx(index) * (1 - start) + start
-    return a.reverse().slice(start, end).times(picki(intervals, index)).compress(picki(rates, index)).repeat(picki(reps, index))
+  loop(100, () => {
+    const start = Math.random()
+    const end = Math.random() * (1 - start) + start
+    return a.scramble().slice(start, end).times(pick(intervals)).compress(pick(rates)).repeat(pick(reps))
   })
 )
 
